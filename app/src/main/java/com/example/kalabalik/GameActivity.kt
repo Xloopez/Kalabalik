9ea58ef9ef062265506807900d54f5e9444dd75a
nicolelopezfeliz <nicole.lopez@live.se>
2020-09-18 22:20:29 +0200

    Fixed loop that shows rounds. Added cards that will flip in activity_game.xml.

--- app/src/main/java/com/example/kalabalik/GameActivity.kt
+++ app/src/main/java/com/example/kalabalik/GameActivity.kt
@@ -4,44 +4,64 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_player.*
+import kotlin.math.round
 
 class GameActivity : AppCompatActivity() {
 
     lateinit var displayPlayerName: TextView
     lateinit var btnNext: Button
-    var counter = 1
-    val  name = GameSettings.listOfPlayers.get(0)//.text.toString()
+    var counter = 0
+    var amountOfRounds = GameSettings.amountOfRounds
+    var currentRound = 1
+    val  name = GameSettings.listOfPlayers.get(counter)//.text.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        displayPlayerName = findViewById(R.id.playerNameTurn)
-        displayPlayerName.setText("$name: s tur!")
+        //Animator object
+
 
         btnNext = findViewById(R.id.btnNextPlayerTurn)
+        displayPlayerName = findViewById(R.id.playerNameTurn)
+        //displayPlayerName.setText("$name: s tur!")
 
         btnNext.setOnClickListener{
             playerTurn()
         }
-
-
-
     }
 
-    fun playerTurn(){
-       val  name = GameSettings.listOfPlayers.get(counter)//.text.toString()
-        when (counter){
-            in 1 until GameSettings.playerCount -> {
-                increaseCounterByOne()
-                displayPlayerName.setText("$name: s tur!")
+    fun playerTurn() {
+        val name = GameSettings.listOfPlayers.get(counter)//.text.toString()
+
+        when (currentRound) {
+            1 -> {
+                Log.d("!!!", "Round $currentRound")
+                increaseRounds()
             }
-            GameSettings.playerCount -> {
+            in 2 until amountOfRounds -> {
+                //Log.d("!!!", "Round $currentRound")
                 increaseCounterByOne()
-                displayPlayerName.setText("$name: s tur!")
+                displayPlayerName.setText("$name:s tur!")
+                //increaseCounterByOne()
+
+                if (counter == GameSettings.playerCount) {
+                    Log.d("!!!", "Round $currentRound")
+                    increaseRounds()
+                    restartcounter()
+                }
+                
+                when (currentRound) {
+                    amountOfRounds -> {
+                        Log.d("!!!", "Last Round!")
+                        displayPlayerName.setText("$name:s tur!")
+                        increaseCounterByOne()
+                    }
+                }
             }
         }
     }
@@ -49,7 +69,12 @@ class GameActivity : AppCompatActivity() {
     fun increaseCounterByOne(){
         counter++
     }
-
+    fun increaseRounds(){
+        currentRound++
+    }
+    fun restartcounter(){
+        counter = 0
+    }
     fun rightClick(points: Int){
 
     }
--- app/src/main/java/com/example/kalabalik/GameSettings.kt
+++ app/src/main/java/com/example/kalabalik/GameSettings.kt
@@ -4,11 +4,12 @@ import android.text.Editable
 
 object GameSettings {
 
-    ///
     var playerCount: Int = 0
+    var amountOfRounds: Int = 15
     var listOfPlayers: MutableList<String> = mutableListOf()
 
-    fun addPlayerToList(player: String){
-            listOfPlayers.add(player)
+    fun addPlayerToList(name: String){
+        //var name = Player(name = playerName)
+        listOfPlayers.add(name)
     }
 }
